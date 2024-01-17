2bc40d6eb226f07f0d4bfc75a01b1ac267af8fd9
SeungHyeon <namsh1125@naver.com>
2024-01-17 22:40:05 +0900

    Init: Spring 초기 설정

---
+++ src/main/kotlin/kea/dpang/eureka/EurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package kea.dpang.eureka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<EurekaServerApplication>(*args)
+}
---
+++ src/test/kotlin/kea/dpang/eureka/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kea.dpang.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
