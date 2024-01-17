044500adf462aa4d9712ad3fe771b88ab4b3da1c
SeungHyeon <namsh1125@naver.com>
2024-01-17 22:53:04 +0900

    Feat: Eureka 서버 기본 설정 추가

--- src/main/kotlin/kea/dpang/eureka/EurekaServerApplication.kt
+++ src/main/kotlin/kea/dpang/eureka/EurekaServerApplication.kt
@@ -2,7 +2,9 @@ package kea.dpang.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class EurekaServerApplication
 
